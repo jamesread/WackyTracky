56ee60544e41416a7f71b43d741d2dc6c1b02e47
jamesread <contact@jwread.com>
2014-09-17 02:14:14 +0000

    Added tool for changing user passwords

---
+++ api/wt-setpasswd.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import argparse 
+import wrapper
+
+parser = argparse.ArgumentParser();
+parser.add_argument('--userid', '-u', required = True)
+parser.add_argument("--password", required = True)
+args = parser.parse_args();
+
+api = wrapper.Wrapper();
+api.changePassword(args.userid, args.password);
